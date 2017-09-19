84a54f2f7a76d016b7700ce3a3140e9f40ead8c4
David Costa <david@m3mediadigital.com.br>
2017-09-19 14:00:57 -0300

    Adicionando o titulo na seção dos pedidos

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="home">
+  <div class="page-content">
+
+  <span class="md-body-1">Últimos Pedidos</span>
 
     <md-list>
       <md-list-item v-for="order in orders" :key="order.id">
